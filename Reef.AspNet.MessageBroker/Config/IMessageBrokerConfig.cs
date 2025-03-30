90085de9559ef77da70ed840df02e48f415718a4
Oleksandr Khomenko <71232792+Alex-Reef@users.noreply.github.com>
2025-03-30 16:57:02 +0300

    Update config

--- Reef.AspNet.MessageBroker/Config/IMessageBrokerConfig.cs
+++ Reef.AspNet.MessageBroker/Config/IMessageBrokerConfig.cs
@@ -3,7 +3,6 @@
 	public interface IMessageBrokerConfig
 	{
 		public string BootstrapServers { get; set; }
-		public string Topic { get; set; }
 		public string GroupId { get; set; }
 		public string SaslUsername { get; set; }
 		public string SaslPassword { get; set; }
--- Reef.AspNet.MessageBroker/Config/MessageBrokerConfig.cs
+++ Reef.AspNet.MessageBroker/Config/MessageBrokerConfig.cs
@@ -3,7 +3,6 @@
 	public class MessageBrokerConfig : IMessageBrokerConfig
 	{
 		public string BootstrapServers { get; set; }
-		public string Topic { get; set; }
 		public string GroupId { get; set; }
 		public string SaslUsername { get; set; }
 		public string SaslPassword { get; set; }
